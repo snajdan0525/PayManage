eb56437d6a8ae86d2a335f8b66ac8a751d2f31ff
sNajdan <snajdan0525@gmail.com>
2017-05-19 16:09:16 +0800

    no message

--- pay/src/main/java/com/paymanage/framework/action/BalancePayAction.java
+++ pay/src/main/java/com/paymanage/framework/action/BalancePayAction.java
@@ -3,15 +3,15 @@ package com.paymanage.framework.action.aciton;
 import android.app.Activity;
 import android.support.v4.util.ArrayMap;
 
-import com.boqii.petlifehouse.pay.model.PayEnum;
-import com.boqii.petlifehouse.pay.model.PayInfo;
+import com.paymanage.framework.model.PayEnum;
+import com.paymanage.framework.model.PayInfo;
 
 
 /**
  * 余额支付
  * Created by snajdan on 2017/5/18.
  */
-public class BalancePayAction extends PayAction {
+public class BalancePayAction extends com.paymanage.framework.action.aciton.PayAction {
 
     public BalancePayAction(Activity mActivity, PayEnum.BqServiceType bqServiceType, ArrayMap<String, String> param, PayResultCallBack mPayResultCallBack) {
         super(mActivity, bqServiceType, PayEnum.EscrowPayType.BQ_BALANCE, param, mPayResultCallBack);
